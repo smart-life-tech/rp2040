5738ffce5f981bcc6d1d5149fdc553b1cde85a59
smart-life-tech <christlightworld@gmail.com>
2023-11-22 08:26:07 +0100

    modified:   lowPower/lowPower.ino

--- lowPower/lowPower.ino
+++ lowPower/lowPower.ino
@@ -6,35 +6,43 @@
 unsigned long lastTouchTime = 0;
 const unsigned long sleepDelay = 10000; // 10 seconds
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(TOUCH_SENSOR_PIN, INPUT);
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(TOUCH_SENSOR_PIN, INPUT);
 }
 
-void loop() {
-  // Check if touch is detected
-  if (digitalRead(TOUCH_SENSOR_PIN) == HIGH) {
-    lastTouchTime = millis(); // Update the last touch time
-    // LED is off, breathe effect
-    for (int i = 0; i < 255; ++i) {
-      analogWrite(LED_PIN, i);
-      delay(5);
+void loop()
+{
+    // Check if touch is detected
+    if (digitalRead(TOUCH_SENSOR_PIN) == HIGH)
+    {
+        lastTouchTime = millis(); // Update the last touch time
+        // LED is off, breathe effect
+        for (int i = 0; i < 255; ++i)
+        {
+            analogWrite(LED_PIN, i);
+            delay(5);
+        }
+        for (int i = 255; i >= 0; --i)
+        {
+            analogWrite(LED_PIN, i);
+            delay(5);
+        }
     }
-    for (int i = 255; i >= 0; --i) {
-      analogWrite(LED_PIN, i);
-      delay(5);
-    }
-  } else {
-    // LED is on, no touch detected
-    analogWrite(LED_PIN, 0); // Turn off the LED
+    else
+    {
+        // LED is on, no touch detected
+        analogWrite(LED_PIN, 0); // Turn off the LED
 
-    // Check if it's time to go to sleep
-    if (millis() - lastTouchTime >= sleepDelay) {
-      // Put the device to sleep
-      LowPower.sleep();
-      // The program resumes from here when it wakes up
-    }
+        // Check if it's time to go to sleep
+        if (millis() - lastTouchTime >= sleepDelay)
+        {
+            // Put the device to sleep
+            LowPower.sleep();
+            // The program resumes from here when it wakes up
+        }
 
-    delay(100);
-  }
+        delay(100);
+    }
 }
