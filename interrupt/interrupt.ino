81e57d5b2c09e3985e9709f4fd5cc603cbd03d47
smart-life-tech <christlightworld@gmail.com>
2023-11-22 08:47:57 +0100

    modified:   interrupt/interrupt.ino

--- interrupt/interrupt.ino
+++ interrupt/interrupt.ino
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
@@ -6,42 +7,53 @@
 
 volatile bool touchDetected = false;
 
-void setup() {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_init(TOUCH_SENSOR_PIN);
-    gpio_set_dir(TOUCH_SENSOR_PIN, GPIO_IN);
-    gpio_pull_up(TOUCH_SENSOR_PIN);
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(TOUCH_SENSOR_PIN, INPUT_PULLUP); // Internal pull-up to keep the pin high when not touched
 
-    // Configure interrupt for falling edge on touch sensor pin
-    gpio_set_irq_enabled_with_callback(TOUCH_SENSOR_PIN, GPIO_IRQ_EDGE_FALL, true, &touchInterrupt);
+  attachInterrupt(digitalPinToInterrupt(TOUCH_SENSOR_PIN), touchInterrupt, FALLING); // Interrupt on falling edge
 
-    sleep_ms(1000); // Allow time for sensor initialization, adjust as needed
+  delay(1000); // Allow time for sensor initialization, adjust as needed
 }
 
-void loop() {
-    // Check if touch is detected
-    if (touchDetected) {
-        touchDetected = false; // Reset the flag
-        // LED is off, breathe effect
-        for (int i = 0; i < 255; ++i) {
-            gpio_put(LED_PIN, i);
-            sleep_ms(5);
-        }
-        for (int i = 255; i >= 0; --i) {
-            gpio_put(LED_PIN, i);
-            sleep_ms(5);
-        }
-    } else {
-        // LED is on, no touch detected
-        gpio_put(LED_PIN, 0); // Turn off the LED
-
-        // Put the device to sleep
-        sleep();
-        // The program resumes from here when it wakes up
+void loop()
+{
+  // Check if touch is detected
+  if (touchDetected)
+  {
+    touchDetected = false; // Reset the flag
+    // LED is off, breathe effect
+    for (int i = 0; i < 255; ++i)
+    {
+      analogWrite(LED_PIN, i);
+      delay(5);
     }
+    for (int i = 255; i >= 0; --i)
+    {
+      analogWrite(LED_PIN, i);
+      delay(5);
+    }
+  }
+  else
+  {
+    // LED is on, no touch detected
+    analogWrite(LED_PIN, 0); // Turn off the LED
+
+    // Put the device to sleep
+    lowPowerSleep();
+    // The program resumes from here when it wakes up
+  }
 }
 
-void touchInterrupt(uint gpio, uint32_t events) {
-    touchDetected = true; // Set the flag to indicate touch detected
+void touchInterrupt()
+{
+  touchDetected = true; // Set the flag to indicate touch detected
+}
+
+// Sleep function for low power mode
+void lowPowerSleep()
+{
+  // Note: Arduino RP2040 core provides the sleep function
+  sleep(1); // Sleep for 1 second, adjust as needed
 }
