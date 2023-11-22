aa23c437cd4175ebbb46676a8d48ca7f3b434059
smart-life-tech <christlightworld@gmail.com>
2023-11-22 08:44:48 +0100

    modified:   interrupt/interrupt.ino

--- interrupt/interrupt.ino
+++ interrupt/interrupt.ino
@@ -1,4 +1,5 @@
-#include <ArduinoLowPower.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
 #define LED_PIN 25
 #define TOUCH_SENSOR_PIN 2
@@ -6,34 +7,41 @@
 volatile bool touchDetected = false;
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(TOUCH_SENSOR_PIN, INPUT_PULLUP); // Internal pull-up to keep the pin high when not touched
-  attachInterrupt(digitalPinToInterrupt(TOUCH_SENSOR_PIN), touchInterrupt, FALLING); // Interrupt on falling edge
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(TOUCH_SENSOR_PIN);
+    gpio_set_dir(TOUCH_SENSOR_PIN, GPIO_IN);
+    gpio_pull_up(TOUCH_SENSOR_PIN);
+
+    // Configure interrupt for falling edge on touch sensor pin
+    gpio_set_irq_enabled_with_callback(TOUCH_SENSOR_PIN, GPIO_IRQ_EDGE_FALL, true, &touchInterrupt);
+
+    sleep_ms(1000); // Allow time for sensor initialization, adjust as needed
 }
 
 void loop() {
-  // Check if touch is detected
-  if (touchDetected) {
-    touchDetected = false; // Reset the flag
-    // LED is off, breathe effect
-    for (int i = 0; i < 255; ++i) {
-      analogWrite(LED_PIN, i);
-      delay(5);
-    }
-    for (int i = 255; i >= 0; --i) {
-      analogWrite(LED_PIN, i);
-      delay(5);
-    }
-  } else {
-    // LED is on, no touch detected
-    analogWrite(LED_PIN, 0); // Turn off the LED
+    // Check if touch is detected
+    if (touchDetected) {
+        touchDetected = false; // Reset the flag
+        // LED is off, breathe effect
+        for (int i = 0; i < 255; ++i) {
+            gpio_put(LED_PIN, i);
+            sleep_ms(5);
+        }
+        for (int i = 255; i >= 0; --i) {
+            gpio_put(LED_PIN, i);
+            sleep_ms(5);
+        }
+    } else {
+        // LED is on, no touch detected
+        gpio_put(LED_PIN, 0); // Turn off the LED
 
-    // Put the device to sleep
-    LowPower.sleep();
-    // The program resumes from here when it wakes up
-  }
+        // Put the device to sleep
+        sleep();
+        // The program resumes from here when it wakes up
+    }
 }
 
-void touchInterrupt() {
-  touchDetected = true; // Set the flag to indicate touch detected
+void touchInterrupt(uint gpio, uint32_t events) {
+    touchDetected = true; // Set the flag to indicate touch detected
 }
